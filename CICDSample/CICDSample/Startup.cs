c9f1d3a2f9e12b2bbc0cba2028c69d7b0aad93da
Ramya Katkuri <ramya.katkuri@pwc.com>
2018-05-17 18:25:15 +0100

    FirstProject

---
+++ CICDSample/CICDSample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CICDSample.Startup))]
+namespace CICDSample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
