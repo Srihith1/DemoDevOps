64131fa6ecf94e939a242ab985104245e7e2474f
Ramya Katkuri <ramya.katkuri@pwc.com>
2018-05-17 18:34:57 +0100

    added selenium tests

---
+++ CICDSample/SeleniumTests/SeleniumTestsScripts.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using Protractor;
+
+namespace SeleniumTests
+{
+    [TestClass]
+    public class SeleniumTestsScripts
+    {
+        private TestContext testContextInstance;
+        private IWebDriver driver;
+        private string appURL;
+
+        [TestMethod]
+        [TestCategory("Chrome")]
+        public void TheBingSearchTest()
+        {
+            driver.Navigate().GoToUrl(appURL + "/");
+            driver.FindElement(By.Id("sb_form_q")).SendKeys("VSTS");
+            driver.FindElement(By.Id("sb_form_go")).Click();
+            driver.FindElement(By.XPath("//ol[@id='b_results']/li/h2/a/strong[3]")).Click();
+            Assert.IsTrue(driver.Title.Contains("VSTS"), "Verified title of the page");
+        }
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [TestInitialize()]
+        public void SetupTest()
+        {
+            appURL = "http://www.bing.com/";
+
+            string browser = "Chrome";
+            var options = new ChromeOptions();
+            options.AddAdditionalCapability("useAutomationExtension", false);
+            options.AddArguments("start-maximized");
+
+            switch (browser)
+            {
+                case "Chrome":
+                    driver = new ChromeDriver(options);
+                    break;
+                default:
+                    driver = new ChromeDriver(options);
+                    break;
+            }
+
+        }
+
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            driver.Quit();
+        }
+    }
+}
+
